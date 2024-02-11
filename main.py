e7d7a4e78dc2c8765f328a71dfc20a6898e0f2d0
HashCode12 <muhammad.hashir1@student.curtin.edu.au>
2024-02-11 19:51:11 +0400

    Made the calculator in Python

--- main.py
+++ main.py
@@ -1,1 +1,50 @@
 # main file for the calculator
+
+
+def main():
+    num1 = int(numbers())
+    num2 = int(numbers())
+    calculation = operation()
+    output = calculate(calculation , num1 , num2)
+    print(output)
+
+
+def numbers():
+    while True:
+        num = input("Number: ")
+
+        if num.isdigit():
+            break
+        else:
+            print("Enter Number again")
+    return num
+
+def operation():
+    while True:
+        operations = input("Enter Operation: ")
+
+        if operations == "+" :
+            break
+        elif operations == "-" :
+            break
+        elif operations == "*" :
+            break
+        elif operations == "/":
+            break
+        else:
+            print("Please enter valid operation...")
+        
+    return operations
+
+def calculate(n , num1 , num2):
+    if n == "+":
+        ans = num1 + num2
+    elif n == "-":
+        ans = num1 - num2
+    elif n == "*":
+        ans = num1 * num2
+    else:
+        ans = num1/num2
+    return ans
+
+main()
